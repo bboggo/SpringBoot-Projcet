9b493a2219e781ee7a263b7dc9b24f119edcdc90
bboggo <kmt0819@naver.com>
2023-02-13 21:09:06 +0900

    [프로젝트 생성] 스프링 부트와 AWS로 혼자 구현하는 웹 서비스

---
+++ SpringBoot_Book/src/test/java/com/book/SpringBoot_Book/SpringBootBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.SpringBoot_Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
